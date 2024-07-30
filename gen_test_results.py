f1f421a7786ef9fb78ab150bb08091fac3eeeead
sieri-logi <sieri@logitech.com>
2024-07-30 09:50:48 +0200

    add summary comments

--- gen_test_results.py
+++ gen_test_results.py
@@ -25,7 +25,7 @@ def gen_test_results():
         {
             "name": "total number of failures",
             "unit": "Failures",
-            "value": 2,
+            "value": 0,
         },
         {
             "name": "total number of tests",
