e3d1e6b0c8d75562e4d60f9c7ff41b7f1efb8fb1
github-action-benchmark <github@users.noreply.github.com>
2024-08-12 13:57:50 +0000

    add Kosmos metrics / tiger board (customSmallerIsBetter) benchmark result for da52710640f9d35079d9c8412c535260d5dcc8ca

--- kosmos/wakanda/tiger/data.js
+++ kosmos/wakanda/tiger/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723470443382,
+  "lastUpdate": 1723471070870,
   "repoUrl": "https://github.com/sieri-logi/benchmark_testing_ground",
   "entries": {
     "Kosmos metrics / tiger board": [
@@ -72,6 +72,78 @@ window.BENCHMARK_DATA = {
             "value": 250,
             "unit": "us",
             "extra": "minimum=124.8us, maximum=500.3us, Delta on max=100.12us, Delta on min = 50.08us, standard deviation=8.2us"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sieri@logitech.com",
+            "name": "sieri-logi",
+            "username": "sieri-logi"
+          },
+          "committer": {
+            "email": "sieri@logitech.com",
+            "name": "sieri-logi",
+            "username": "sieri-logi"
+          },
+          "distinct": true,
+          "id": "da52710640f9d35079d9c8412c535260d5dcc8ca",
+          "message": "add new run on pemu node with old school",
+          "timestamp": "2024-08-12T15:57:32+02:00",
+          "tree_id": "46617764814d4bed32ea07776a96b403bf1024fc",
+          "url": "https://github.com/sieri-logi/benchmark_testing_ground/commit/da52710640f9d35079d9c8412c535260d5dcc8ca"
+        },
+        "date": 1723471070609,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pytestbox.device.hid.latency.performance.LatencyPerformance4kHzUSBTestCase.test_measure_break_latency_in_active_mode : The switch latency time : (average)",
+            "value": 1628.7740000000003,
+            "unit": "us",
+            "extra": "minimum=1447.74us, maximum=1807.56us, 95_percentile=1758.469us, 99_percentile=1783.7802000000001us"
+          },
+          {
+            "name": "pytestbox.device.hid.latency.performance.LatencyPerformance4kHzUSBTestCase.test_measure_break_latency_with_hybrid_switch_in_power_save_mode_in_active_mode : The switch latency time : (average)",
+            "value": 375.3858,
+            "unit": "us",
+            "extra": "minimum=212.72us, maximum=542.38us, 95_percentile=507.967us, 99_percentile=533.2720000000002us"
+          },
+          {
+            "name": "pytestbox.device.hid.latency.performance.LatencyPerformance4kHzUSBTestCase.test_measure_make_latency_default_switch_in_active_mode : The switch latency time : (average)",
+            "value": 372.8330000000001,
+            "unit": "us",
+            "extra": "minimum=200.54us, maximum=547.78us, 95_percentile=498.38100000000003us, 99_percentile=516.1198000000002us"
+          },
+          {
+            "name": "pytestbox.device.hid.latency.performance.LatencyPerformance4kHzUSBTestCase.test_measure_make_latency_in_lift_mode : The switch latency time : (average)",
+            "value": 403.39119999999997,
+            "unit": "us",
+            "extra": "minimum=257.62us, maximum=508.04us, 95_percentile=507.881us, 99_percentile=507.98060000000004us"
+          },
+          {
+            "name": "pytestbox.device.hid.latency.performance.LatencyPerformance4kHzUSBTestCase.test_measure_make_latency_with_hybrid_switch_in_power_save_mode_in_active_mode : The switch latency time : (average)",
+            "value": 370.17839999999995,
+            "unit": "us",
+            "extra": "minimum=212.62us, maximum=549.44us, 95_percentile=513.933us, 99_percentile=549.044us"
+          },
+          {
+            "name": "pytestbox.device.hid.latency.performance.LatencyPerformance4kHzUSBTestCase.test_x_motion_in_run_mode : The motion latency time : (average)",
+            "value": 343.23690000000005,
+            "unit": "us",
+            "extra": "minimum=162.22us, maximum=533.94us, 95_percentile=518.1320000000001us, 99_percentile=532.026us"
+          },
+          {
+            "name": "pytestbox.device.hid.latency.performance.LatencyPerformance4kHzUSBTestCase.test_y_motion_in_run_mode : The motion latency time : (average)",
+            "value": 348.7837,
+            "unit": "us",
+            "extra": "minimum=161.88us, maximum=528.44us, 95_percentile=521.737us, 99_percentile=528.0414us"
+          },
+          {
+            "name": "pytestbox.device.hid.mouse.displacement.performance.XYDisplacementReportRatePerformanceUSBTestCase.test_4kHz_report_rate_stability : The report rate : ",
+            "value": 250,
+            "unit": "us",
+            "extra": "minimum=124.8us, maximum=375.4us, Delta on max=50.15999999999999us, Delta on min = 50.08us, standard deviation=6.2us"
           }
         ]
       }
